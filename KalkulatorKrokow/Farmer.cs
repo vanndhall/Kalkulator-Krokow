626d7b616bc61fb0eb741fa4740be049892bc0a7
vanndhall <szymon.mantey@wp.pl>
2018-02-14 21:35:35 +0100

    rep1

---
+++ KalkulatorKrokow/Farmer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorKrokow
+{
+    class Farmer
+    {
+        public int BagsOfFeed;
+        public const int FeedMultiplier = 30;
+
+        private int numberOffCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOffCows;
+            }
+            set
+            {   numberOffCows = value;
+                BagsOfFeed = numberOffCows * FeedMultiplier;
+            }
+        }
+    }
+}
--- KalkulatorKrokow/Form1.Designer.cs
+++ KalkulatorKrokow/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.calculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ilość kroków:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(101, 57);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Tag = "";
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(101, 83);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(120, 23);
+            this.calculate.TabIndex = 2;
+            this.calculate.Text = "Oblicz";
+            this.calculate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 137);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button calculate;
     }
 }
 
